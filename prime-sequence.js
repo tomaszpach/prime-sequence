514cfc4019df82810edc830eec8929e568d4252d
tomaszpach <tomaszpach@icloud.com>
2018-11-11 15:46:05 +0100

    avoid unexpected coerction

--- prime-sequence.js
+++ prime-sequence.js
@@ -9,7 +9,7 @@
     function isPrime(n) {
         if (n < 2 || n % 1) return false;
         for (let i = 2; i < n; i++) {
-            if (n % i == 0) return false;
+            if (n % i === 0) return false;
         }
         return true;
     }
