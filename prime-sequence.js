a19817a04019d90db787b400ef00434515de4f66
tomaszpach <tomaszpach@icloud.com>
2018-11-11 15:44:46 +0100

    cleanup

--- prime-sequence.js
+++ prime-sequence.js
@@ -1,15 +1,15 @@
 (() => {
     console.clear();
 
-    const A = [2,3,9,2,5,1,3,7,10];
-    const B = [2,1,3,4,3,10,6,6,1,7,10,10,10];
-    const C = [2,9,2,5,7,10];
+    const A = [2, 3, 9, 2, 5, 1, 3, 7, 10];
+    const B = [2, 1, 3, 4, 3, 10, 6, 6, 1, 7, 10, 10, 10];
+    const C = [2, 9, 2, 5, 7, 10];
 
     // Slow, brute force
     function isPrime(n) {
-        if(n<2 || n%1) return false;
-        for(let i=2; i<n; i++) {
-            if (n%i==0) return false;
+        if (n < 2 || n % 1) return false;
+        for (let i = 2; i < n; i++) {
+            if (n % i == 0) return false;
         }
         return true;
     }
@@ -17,9 +17,9 @@
     function createOccurrenceIndex(arr) {
         const index = {};
 
-        for(let i=0, n=arr.length; i<n; i++) {
+        for (let i = 0, n = arr.length; i < n; i++) {
             const key = arr[i];
-            if(index.hasOwnProperty(key)) {
+            if (index.hasOwnProperty(key)) {
                 index[key]++;
             } else {
                 index[key] = 1;
@@ -32,8 +32,8 @@
     function filterIndexByValue(index, filter) {
         const filtered = {};
 
-        for(let key in index) {
-            if(filter.call(null, index[key])) {
+        for (let key in index) {
+            if (filter.call(null, index[key])) {
                 filtered[key] = index[key];
             }
         }
@@ -41,7 +41,7 @@
         return filtered;
     }
 
-    function sequence(a,b) {
+    function sequence(a, b) {
         let index = createOccurrenceIndex(b);
         index = filterIndexByValue(index, isPrime);
 
@@ -49,17 +49,17 @@
     }
 
     function isArraysEqual(arr1, arr2) {
-        if(arr1.length !== arr2.length)
+        if (arr1.length !== arr2.length)
             return false;
-        for(var i = arr1.length; i--;) {
-            if(arr1[i] !== arr2[i])
+        for (var i = arr1.length; i--;) {
+            if (arr1[i] !== arr2[i])
                 return false;
         }
 
         return true;
     }
 
-    console.log('it is:', sequence(A,B));
+    console.log('it is:', sequence(A, B));
     console.log('should be:', C);
     console.log('is the same?', isArraysEqual(sequence(A, B), C))
 
